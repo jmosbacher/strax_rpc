6b416a542a6c45cdc109bc26028b60bd986ffcb3
Yossi Mosbacher <joe.mosbacher@gmail.com>
2018-09-10 13:47:35 +0300

    Proof of concept

---
+++ strax_rpc_client.py
@@ -0,0 +1,46 @@
+
+from __future__ import print_function
+
+import random
+
+import grpc
+
+import strax_rpc_pb2
+import strax_rpc_pb2_grpc
+
+from prettytable import PrettyTable
+
+def search_field(stub, pattern, max_matches=10):
+    sp = strax_rpc_pb2.SearchPattern(
+        pattern=pattern,
+        max_matches=max_matches,
+    )
+    rs = stub.SearchField(sp)
+    for r in rs:
+        print(f"{r.column} is part of {r.dataname} (provided by {r.plugin})")
+
+def data_info(stub, dataname):
+    dn = strax_rpc_pb2.DataName(name=dataname)
+    rs = stub.DataInfo(dn)
+    t = PrettyTable()
+    t.field_names = ['index','Data name', 'Field name', 'Comment']
+    for r in rs:
+        t.add_row([r.index, r.name, r.dtype, r.comment ])
+    print(t)
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = strax_rpc_pb2_grpc.StraxRPCStub(channel)
+        print("-------------------- Search for s1* --------------------")
+        search_field(stub, "s1*")
+        print("\n-------------- Data Info for event_basics --------------\n")
+        data_info(stub, 'event_basics')
+
+
+
+if __name__ == '__main__':
+    run()
---
+++ strax_rpc_server.py
@@ -0,0 +1,91 @@
+"""The Python implementation of the gRPC route guide server."""
+
+from concurrent import futures
+import numpy as np
+import pandas as pd
+import strax
+import time
+import math
+import fnmatch
+import grpc
+
+import strax_rpc_pb2
+import strax_rpc_pb2_grpc
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+ctx = strax.Context(
+    storage=[strax.ZipDirectory('/home/yossi/Sandbox/processed'),
+             strax.DataDirectory('/home/yossi/Sandbox/custom_data')],
+    register_all=strax.xenon.plugins)
+
+def search_field(ctx, pattern, max_matches):
+    """
+    Temporary fix
+    """
+    match_list = []
+    cache = dict()
+    for d in ctx._plugin_class_registry:
+        if d not in cache:
+            cache.update(ctx._get_plugins((d,), run_id='0'))
+        p = cache[d]
+
+        for field_name in p.dtype.names:
+            if fnmatch.fnmatch(field_name, pattern):
+                if max_matches and len(match_list)>=max_matches:
+                    return match_list
+                match_list.append((field_name, d, p.__class__.__name__))
+    return match_list
+
+class StraxRPCServicer(strax_rpc_pb2_grpc.StraxRPCServicer):
+
+    def __init__(self):
+        self.ctx = ctx
+
+    def SearchField(self, request, context):
+        """
+            TODO: Adapt search_field to accept flag for api usage of strax
+            then change this method to:
+                        match_list = self.ctx.search_field(pattern)
+        """
+
+        pattern = request.pattern
+        max_matches = request.max_matches
+        match_list = search_field(self.ctx, pattern, max_matches)
+        for column, dataname, plugin in match_list:
+            yield strax_rpc_pb2.Match(
+                  column=column,
+                  dataname=dataname,
+                  plugin=plugin,
+                 )
+
+
+    def DataInfo(self, request, context):
+      """
+
+      """
+      dataname = request.name
+      df = self.ctx.data_info(dataname)
+      for idx,row in df.iterrows():
+          yield strax_rpc_pb2.Field(
+                index=idx,
+                name=row['Field name'],
+                dtype=str(row['Data type']),
+                comment=row['Comment'],
+               )
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    strax_rpc_pb2_grpc.add_StraxRPCServicer_to_server(
+        StraxRPCServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
